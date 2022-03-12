888e69113e00093b9c3d756534b3eb9b2229a54b
hinesqui <hinesqui@gmail.com>
2022-03-12 20:53:17 +0300

    batch execute/speed up

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,20 +1,26 @@
 import dataclasses
 import os
 import sqlite3
+from typing import Generator
 
 import psycopg2
+from attr import dataclass
 from dotenv import load_dotenv
+from entities import Filmwork, Genre, GenreFilmwork, Person, PersonFilmwork
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
 
-from entities import Filmwork, Genre, Person, GenreFilmwork, PersonFilmwork
-
-TABLES = ('film_work', 'genre', 'person', 'genre_film_work', 'person_film_work')
-DATACLASSES = dict(zip(TABLES, (Filmwork, Genre, Person, GenreFilmwork, PersonFilmwork)))
+TABLES = {
+    'film_work': Filmwork,
+    'genre': Genre,
+    'person': Person,
+    'genre_film_work': GenreFilmwork,
+    'person_film_work': PersonFilmwork
+}
 # Load environments
 load_dotenv()
 
-dsl = {
+DSL = {
     'dbname': os.environ.get('DB_NAME'),
     'user': os.environ.get('DB_USER'),
     'password': os.environ.get('DB_PASSWORD'),
@@ -22,66 +28,63 @@ dsl = {
     'port': os.environ.get('DB_PORT', 5432)
 }
 
-sqlite_db_path = 'db.sqlite'
+BATCH = 500
+SQLITE_DB_PATH = 'db.sqlite'
 
 
 class SQLiteLoader:
     def __init__(self, connection):
         self.con = connection
 
-    def load_movies(self) -> dict:
-        data = {}
+    def load_movies(self, table, model) -> Generator:
         cursor = self.con.cursor()
-        for table in TABLES:
-            film_work_columns = 'id, title, description, creation_date, rating, type, created_at, updated_at'
-            columns = '*' if table != 'film_work' else film_work_columns
-            cursor.execute('SELECT {0} FROM {1}'.format(columns, table))
-            rows = cursor.fetchall()
+        cursor.execute('SELECT {0} FROM {1}'.format(model.sqlite_columns(), table))
 
-            self._make_rows_pretty(rows, table, data)
-
-        return data
+        while rows := cursor.fetchmany(BATCH):
+            yield self._make_rows_pretty(rows, model)
 
     @staticmethod
-    def _make_rows_pretty(rows: list, table: str, data: dict):
+    def _make_rows_pretty(rows, model: dataclass):
         dataclasses = []
         for row in rows:
-            dataclasses.append(DATACLASSES[table](*row))
+            dataclasses.append(model(*row))
 
-        data[table] = dataclasses
+        return dataclasses
 
 
 class PostgresSaver:
     def __init__(self, connection):
         self.con = connection
 
-    def save_all_data(self, data: dict):
+    def save_all_data(self, data: Generator, table, model):
         cursor = self.con.cursor()
-        for table in TABLES:
-            columns = tuple(DATACLASSES[table].__annotations__.keys())
-            columns_pretty = ', '.join(columns)
-            mogrify_pattern = ', '.join(['%s'] * len(columns))
-            args = ','.join(
-                cursor.mogrify(f'({mogrify_pattern})', dataclasses.astuple(item)).decode() for item in data[table]
-            )
-            cursor.execute(f"""
-                            INSERT INTO content.{table} ({columns_pretty})
-                            VALUES {args}
-                            ON CONFLICT (id) DO NOTHING
-                            """)
+        columns = tuple(model.__annotations__.keys())
+        columns_pretty = ', '.join(columns)
+        mogrify_pattern = ', '.join(['%s'] * len(columns))
+        args = ','.join(
+            cursor.mogrify(f'({mogrify_pattern})', dataclasses.astuple(item)).decode() for item in data
+        )
+        cursor.execute(f"""
+                        INSERT INTO content.{table} ({columns_pretty})
+                        VALUES {args}
+                        ON CONFLICT (id) DO NOTHING
+                        """)
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     """Основной метод загрузки данных из SQLite в Postgres"""
     postgres_saver = PostgresSaver(pg_conn)
     sqlite_loader = SQLiteLoader(connection)
-
-    data = sqlite_loader.load_movies()
-    postgres_saver.save_all_data(data)
+    for table, model in TABLES.items():
+        data = sqlite_loader.load_movies(table, model)
+        [postgres_saver.save_all_data(i, table, model) for i in data]
 
 
 if __name__ == '__main__':
     psycopg2.extras.register_uuid()
 
-    with sqlite3.connect(sqlite_db_path) as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+    with sqlite3.connect(SQLITE_DB_PATH) as sqlite_conn, psycopg2.connect(**DSL, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
+
+    sqlite_conn.close()
+    pg_conn.close()
